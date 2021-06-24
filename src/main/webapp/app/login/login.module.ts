40bfe5caee7ce093b7a9e6eaa4cbbd3e5dc2bb48
Mili <72173922+circunferenzoide@users.noreply.github.com>
2021-06-24 10:38:26 -0300

    Se cambiaron cosas para las pruebas de Tortoise

--- src/main/webapp/app/login/login.module.ts
+++ src/main/webapp/app/login/login.module.ts
@@ -5,8 +5,3 @@ import { SharedModule } from 'app/shared/shared.module';
 import { LOGIN_ROUTE } from './login.route';
 import { LoginComponent } from './login.component';
 
-@NgModule({
-  imports: [SharedModule, RouterModule.forChild([LOGIN_ROUTE])],
-  declarations: [LoginComponent],
-})
-export class LoginModule {}
